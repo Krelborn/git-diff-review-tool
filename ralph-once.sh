a4723423bc166b1316ddda153964dfd73f8fc4ed
Matt Styles <matt.styles@gmail.com>
2026-03-28 16:54:18 +0000

    Few stray files need checking in

--- ralph-once.sh
+++ ralph-once.sh
@@ -1,11 +1,11 @@
 set -e
 
-claude --dangerously-skip-permissions "@docs/requirements @docs/technical-design @docs/mockups/main-ui.html @progress.md \
+claude --permission-mode acceptEdits "@docs/requirements @docs/technical-design @docs/mockups/main-ui.html @progress.md \
 1. Find the highest priority task to work on and work only on that task. \
 This should be the one YOU decide has the highest priority, not necessarily the one that is first in the list. \
 2. Check that the types check and the tests pass. \
-3. Update the requirements document with the work that was done. \
-4. Update the progress document with the work that was done. \
+3. Update the requirements document and github issue with the work that was done. \
+4. Update the progress document with the work that was done and mark the github issue as complete. \
 Use this to leave a note for the next person who works on this project. \
 5. Make a git commit of the work that was done. \
 ONLY WORK ON A SINGLE TASK. \
